7710d4ec8c2c8a25ae3c2607e1c52536fb66bce8
Tshaba Phomolo Benedict <benedicttshaba@gmail.com>
2018-01-24 13:20:21 +0200

    Fix logout page

--- logout.php
+++ logout.php
@@ -1,18 +1,21 @@
 <?php
-session_start();
-?>
-<!DOCTYPE html>
-<html>
-<body>
-<?php
-// remove all session variables
-session_unset();
-// destroy the session
-session_destroy();
-echo '<script type="text/javascript">
-           window.location = "index.php"
-      </script>'
+	session_start();
+	session_unset(); // remove all session variables
+	session_destroy(); // destroy the session
 ?>
 
-</body>
-</html>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<meta name="author" content="Tshaba Phomolo Benedict" >
+	<meta http-equiv="refresh" content="2;url=index.php"/>
+	<title>HMS | Logout</title>
+</head>
+<body>
+	<header>
+		<h1 class="dark">You have successfully logged out!</h1>
+	</header>
+
+<?php include('footer.php'); ?>
