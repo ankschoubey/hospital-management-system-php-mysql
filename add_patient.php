b70a60733846c4cbc99130440f942297e30207a5
Tshaba Phomolo Benedict <benedicttshaba@gmail.com>
2018-01-28 13:03:01 +0200

    Fix Undefined index error

--- add_patient.php
+++ add_patient.php
@@ -10,12 +10,12 @@
 	noAccessForAdmin();
 	noAccessIfNotLoggedIn();
  
-	if($_SESSION["user-type"] != 'normal'){
+	if(isset($_SESSION["user-type"]) && $_SESSION["user-type"] != 'normal'){
 		include("nav-bar.php");
 	}
 ?>
 <div class="container">
-  <h2>Welcome, <?php echo $_SESSION["fullname"];?>!</h2>
+  <h2>Welcome, <?php echo isset($_SESSION["fullname"]) ? $_SESSION["fullname"] : "No User";?>!</h2>
       <div class='alert alert-info'>
               <strong>Info!</strong> Appointment will be booked only for today - <?php echo date("d/m/y"); ?>. Appointment time will be between 10:30 to 3:30 or 4:30 to 9:30 once appointment is booked.</div>
       <h3>Enter Information</h3>
